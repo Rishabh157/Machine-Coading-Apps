2b81c031af431bc523633e27496e66980deaf580
Rishabh157 <rishabhgour157@gmail.com>
2026-04-17 03:08:38 +0530

    added the heading

--- src/screens/ToggleSwitch.tsx
+++ src/screens/ToggleSwitch.tsx
@@ -10,21 +10,25 @@ const ToggleSwitch = () => {
   console.log("active", active);
 
   return (
-    <div className="flex justify-center mt-10">
-      <div className="relative h-10 bg-green-500 w-20 rounded-full flex items-center gap-x-1 px-2 py-2 ">
-        <span
-          className={`text-white font-bold select-none absolute ${active ? "right-2" : "left-2"} `}
-        >
-          {!active ? "YES" : "NO"}
-        </span>
+    <div>
+      <h1 className="text-center text-lg mt-4">
+        Build a Toggle Switch Component
+      </h1>
+      <div className="flex justify-center mt-10">
+        <div className="relative h-10 bg-green-500 w-20 rounded-full flex items-center gap-x-1 px-2 py-2 ">
+          <span
+            className={`text-white font-bold select-none absolute ${active ? "right-2" : "left-2"} `}
+          >
+            {!active ? "YES" : "NO"}
+          </span>
 
-        <div
-          onClick={handleChange}
-          //   className={`absolute h-8 w-8 rounded-full bg-white text-white cursor-pointer transition-all ${active ? "right-1" : "left-1"}`}
-          className={`absolute h-8 w-8 rounded-full bg-white cursor-pointer transition-all duration-300 ${
-            active ? "translate-x-0" : "translate-x-8"
-          }`}
-        ></div>
+          <div
+            onClick={handleChange}
+            className={`absolute h-8 w-8 rounded-full bg-white cursor-pointer transition-all duration-300 ${
+              active ? "translate-x-0" : "translate-x-8"
+            }`}
+          ></div>
+        </div>
       </div>
     </div>
   );
